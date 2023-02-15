69bb7f1315e3bd7952b2b4bed90c8014339da620
Lam Thinh Phat <lamthinhphat2001@gmail.com>
2023-02-15 11:40:18 +0700

    simple fix

--- lib/constants/api_const.dart
+++ lib/constants/api_const.dart
@@ -1,6 +1,6 @@
 
 
 class ApiConst {
-  static String APIKey = "";
-  static String OrgID = "";
+  static String APIKey = "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";
+  static String OrgID = "org-eGtKRhgKTXsYS1BpEV7I1ZVF";
 }
