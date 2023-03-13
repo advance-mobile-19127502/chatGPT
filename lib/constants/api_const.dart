0c49cc8207b5fa96524dfefd76901ac91289cb0d
Lam Thinh Phat <lamthinhphat2001@gmail.com>
2023-03-13 15:48:16 +0700

    Remove API Key

--- lib/constants/api_const.dart
+++ lib/constants/api_const.dart
@@ -1,6 +1,6 @@
 
 
 class ApiConst {
-  static String APIKey = "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";
-  static String OrgID = "org-eGtKRhgKTXsYS1BpEV7I1ZVF";
+  static String APIKey = "";
+  static String OrgID = "";
 }
